ebb6f538b0d9e302c9dba53f452d813f9107ae48
IHiFiveI <subject6gfrt@gmail.com>
2020-09-11 15:35:04 +0700

    Visual Studio initialized

---
+++ LiaRsLaiR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiaRsLaiR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
